4608b4197eb6daa1863436e9c999be95e632cd78
Ian Frost <inf5000@gmail.com>
2015-09-14 15:04:33 -0400

    Added JS and JQuery for flyout to work on click

--- source/js/init.js
+++ source/js/init.js
@@ -13,16 +13,27 @@ var jewelryFlyout = document.querySelectorAll('.jewelry-image > a');
 $('.jewelry-image > a').click(function(event) {
 	event.preventDefault();
 	var jewelryId = $(this).attr('href');
-	var jewelryCurrent = $(jewelryId);	
-	$('.jewelry-panel').not(jewelryCurrent).removeClass('active');
+	var jewelryCurrent = $(this).parents('.jewelry-item');
+	console.log(jewelryCurrent);
+	var xOffsetFromWindow = getOffsetLeft($(this)[0]);
+	$('.jewelry-item').not(jewelryCurrent).removeClass('active');
 	if(jewelryCurrent.hasClass('active')) {
 		jewelryCurrent.removeClass('active');
 	}
 	else {
 		jewelryCurrent.addClass('active');
+		jewelryCurrent.find('.jewelry-panel').css("left",(xOffsetFromWindow * -1)+"px");
 	}
-
 });
 
-
-
+function getOffsetLeft( elem )
+{
+    var offsetLeft = 0;
+    do {
+      if ( !isNaN( elem.offsetLeft ) )
+      {
+          offsetLeft += elem.offsetLeft;
+      }
+    } while( elem = elem.offsetParent );
+    return offsetLeft;
+}
