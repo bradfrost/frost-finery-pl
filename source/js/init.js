55e77ea2bdfb704ccae9d825018c480830ec38a2
Ian Frost <inf5000@gmail.com>
2015-09-14 13:11:44 -0400

    Added JS and JQuery to allow for flyout to turn on and off on click

--- source/js/init.js
+++ source/js/init.js
@@ -7,3 +7,22 @@
 	});
 	
 })(this);
+
+flyoutMode = false;
+var jewelryFlyout = document.querySelectorAll('.jewelry-image > a');
+$('.jewelry-image > a').click(function(event) {
+	event.preventDefault();
+	var jewelryId = $(this).attr('href');
+	var jewelryCurrent = $(jewelryId);	
+	$('.jewelry-panel').not(jewelryCurrent).removeClass('active');
+	if(jewelryCurrent.hasClass('active')) {
+		jewelryCurrent.removeClass('active');
+	}
+	else {
+		jewelryCurrent.addClass('active');
+	}
+
+});
+
+
+
