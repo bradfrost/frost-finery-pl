a5e9b7c629dae2c624248d061627e9a02dcb8154
Ian Frost <inf5000@gmail.com>
2015-09-22 16:34:55 -0400

    Added JS to position window to show item and panel below on click

--- source/js/init.js
+++ source/js/init.js
@@ -79,6 +79,9 @@ function openPanel(id) {
 	if (isLooping === false) {
 		startInterval();
 	}
+	var yOffset = getOffsetTop(jewelryId[0]); 
+	console.log("Offset Top: " + yOffset);
+	window.scrollTo(0, (yOffset-($('.jewelry-thumb').height())));
 }
 	
 function getOffsetLeft( elem )
@@ -92,5 +95,18 @@ function getOffsetLeft( elem )
     } while( elem = elem.offsetParent );
     return offsetLeft;
 }
+
+function getOffsetTop( elem )
+{
+    var offsetTop = 0;
+    do {
+      if ( !isNaN( elem.offsetTop ) )
+      {
+          offsetTop += elem.offsetTop;
+      }
+    } while( elem = elem.offsetParent );
+    return offsetTop;
+}
+
 
 
